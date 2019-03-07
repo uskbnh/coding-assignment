f9319461f0d42c97afd13b4f907e5619d42cb7c5
kaahi <xiongzilun@gmail.com>
2019-03-07 14:40:51 -0500

    correct feedback 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import './App.css';
+
 import MyList from './components/MyList';
 import Recommendations from './components/Recommendations';
+
+import './App.css';
+import logo from './images/logo.png';
 
 class App extends Component {
    render() {
       return (
          <div className="App">
-            <MyList />
-            <hr />
-            <Recommendations />
+            <header>
+               <img className='Logo' src={logo} alt='Netflex'/>
+            </header>
+            <div className="AppList">
+               <MyList />
+               <hr />
+               <Recommendations />
+            </div>
          </div>
       );
    }
