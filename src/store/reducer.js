c294f70b5e8599d1225e824ffa8fb8f54c04b250
kaahi <xiongzilun@gmail.com>
2019-03-09 19:38:10 -0500

    refine useless code

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,39 +1,23 @@
 const initState = {
-    'mylist':[  
-        
-     ],
-     'recommendations':[  
-        
-     ]
+    'mylist':[],
+    'recommendations':[]
 }
 
 const reducer = (state = initState, action) => {
-    let newMyList = null;
-    let newRecommendations = null;
     switch (action.type) {
          case 'INIT':
             return action.payload
          
          case 'ADD':
-            newMyList = [...state.mylist];
-            newRecommendations = [...state.recommendations];
-            let addItem = state.recommendations.filter(item => item.id === action.payload.id)[0];
-            newMyList.push(addItem);
-            newRecommendations = newRecommendations.filter(item => item.id !== action.payload.id);
             return {
-                'recommendations':newRecommendations,
-                'mylist':newMyList
+                'recommendations':state.recommendations.filter(item => item.id !== action.payload.id),
+                'mylist':state.mylist.concat(state.recommendations.filter(item => item.id === action.payload.id)[0])
             }
 
          case 'DEL':
-            newMyList = [...state.mylist];
-            newRecommendations = [...state.recommendations];
-            newMyList = newMyList.filter(item => item.id !== action.payload.id);
-            let returnItem = state.mylist.filter(item => item.id === action.payload.id)[0];
-            newRecommendations.push(returnItem);
             return {
-                'recommendations':newRecommendations,
-                'mylist':newMyList
+                'recommendations':state.recommendations.concat(state.mylist.filter(item => item.id === action.payload.id)[0]),
+                'mylist':state.mylist.filter(item => item.id !== action.payload.id)
             }
         default:
             return state;
